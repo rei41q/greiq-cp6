9e051c5b76f8f86a6400a5b0ca979130bbf15b59
Rei Greiq <greiqrei@gmail.com>
2022-07-19 21:47:18 +0700

    update seeders

---
+++ src/database/seeders/20220719141726-create-super-user.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert("Users", [
+
+      {
+      
+      username: "superuser",
+      
+      password: "iamsuperuser",
+      
+      createdAt: new Date(),
+      
+      updatedAt: new Date(),
+         
+      is_active: true,
+      
+      },
+      
+      ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('Users', null, {});
+  }
+};
