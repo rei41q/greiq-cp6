87e07781cbba4ffb8b2d31d5cffea828a9c09837
Rei Greiq <greiqrei@gmail.com>
2022-07-19 22:25:39 +0700

    product beautiful structured

--- src/database/seeders/20220719141950-create-super-product.js
+++ src/database/seeders/20220719141950-create-super-product.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -10,80 +10,72 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-     await queryInterface.bulkInsert("Products", [
-
+     */
+    await queryInterface.bulkInsert("Products", [
       {
-      
-      product_name: "Vitamin C1000",
-      
-      deskripsi: "Meningkatkan kekebalan tubuh",
-      
-      stok: 5,
-      
-      tersedia:true,
+        product_name: "Vitamin C1000",
+
+        deskripsi: "Meningkatkan kekebalan tubuh",
+
+        stok: 5,
+
+        tersedia: true,
       },
       {
-      
         product_name: "Lemon tea",
-        
+
         deskripsi: "Meningkatkan..",
-        
+
         stok: 2,
-        
-        tersedia:true,
-        },
-        {
-      
-          product_name: "Frisian Flag",
-          
-          deskripsi: "Meningkatkan..",
-          
-          stok: 0,
-          
-          tersedia:false,
-          },
-          {
-      
-            product_name: "Milo",
-            
-            deskripsi: "Meningkatkan..",
-            
-            stok: 11,
-            
-            tersedia:true,
-            },
-            {
-      
-              product_name: "Pulpy",
-              
-              deskripsi: "Meningkatkan..",
-              
-              stok: 0,
-              
-              tersedia:false,
-              },
-              {
-      
-                product_name: "Torabika Cappuccino",
-                
-                deskripsi: "Meningkatkan..",
-                
-                stok: 13,
-                
-                tersedia:true,
-                },
-      
-      ]);
+
+        tersedia: true,
+      },
+      {
+        product_name: "Frisian Flag",
+
+        deskripsi: "Meningkatkan..",
+
+        stok: 0,
+
+        tersedia: false,
+      },
+      {
+        product_name: "Milo",
+
+        deskripsi: "Meningkatkan..",
+
+        stok: 11,
+
+        tersedia: true,
+      },
+      {
+        product_name: "Pulpy",
+
+        deskripsi: "Meningkatkan..",
+
+        stok: 0,
+
+        tersedia: false,
+      },
+      {
+        product_name: "Torabika Cappuccino",
+
+        deskripsi: "Meningkatkan..",
+
+        stok: 13,
+
+        tersedia: true,
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-     await queryInterface.bulkDelete('Products', null, {});
-  }
+    await queryInterface.bulkDelete("Products", null, {});
+  },
 };
